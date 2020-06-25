6bbc5e0fea3c4ba14eaf12aca2d4f3c1890dac6f
Hamish <hamish.zou@outlook.com>
2020-06-25 23:41:56 +0800

    force push to current operating scripts

---
+++ rsn-archive/edit.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+import json
+import os
+import re
+import time
+from datetime import datetime
+
+import mwparserfromhell
+os.environ['PYWIKIBOT_DIR'] = os.path.dirname(os.path.realpath(__file__))
+import pywikibot
+from config import config_page_name  # pylint: disable=E0611,W0614
+
+os.environ['TZ'] = 'UTC'
+print('Starting at: ' + time.asctime(time.localtime(time.time())))
+
+site = pywikibot.Site()
+site.login()
+
+config_page = pywikibot.Page(site, config_page_name)
+cfg = config_page.text
+cfg = json.loads(cfg)
+print(json.dumps(cfg, indent=4, ensure_ascii=False))
+
+if not cfg["enable"]:
+    exit("disabled\n")
+
+rsnpage = pywikibot.Page(site, cfg["main_page_name"])
+text = rsnpage.text
+
+wikicode = mwparserfromhell.parse(text)
+
+archivelist = {}
+count = 0
+
+for section in wikicode.get_sections()[1:]:
+    if section == '':
+        continue
+    else:
+        title = str(section.get(0).title)
+        print(title, end="\t")
+
+    processed = False
+    publicizing = False
+
+    for template in section.filter_templates():
+        if template.name.lower() == "status2":
+            if template.has(1):
+                status = template.get(1)
+            else:
+                status = "(empty)"
+            print("status", status, end="\t")
+            if status in cfg["publicizing_status"]:
+                publicizing = True
+                print("publicizing", end="\t")
+                break
+            elif status in cfg["done_status"]:
+                processed = True
+                print("processed", end="\t")
+                break
+            else:
+                print("not processed", end="\t")
+                break
+
+    lasttime = datetime(1, 1, 1)
+    for m in re.findall(r"(\d{4})年(\d{1,2})月(\d{1,2})日 \(.\) (\d{2}):(\d{2}) \(UTC\)", str(section)):
+        d = datetime(int(m[0]), int(m[1]), int(m[2]), int(m[3]), int(m[4]))
+        lasttime = max(lasttime, d)
+    print(lasttime, end="\t")
+
+    if (
+        (
+            (processed and not publicizing and time.time() - lasttime.timestamp() > cfg["time_to_live_for_processed"])
+            or (not processed and not publicizing and time.time() - lasttime.timestamp() > cfg["time_to_live_for_not_processed"])
+        )
+            and lasttime != datetime(1, 1, 1)):
+        target = (lasttime.year, lasttime.month)
+        if target not in archivelist:
+            archivelist[target] = []
+        archivestr = str(section).strip()
+        archivestr = re.sub(
+            r"{{bot-directive-archiver\|no-archive-begin}}[\s\S]+?{{bot-directive-archiver\|no-archive-end}}\n?", "", archivestr)
+        archivelist[target].append(archivestr)
+        count += 1
+        section.remove(section)
+        print("archive to " + str(target), end="\t")
+    print()
+
+text = str(wikicode)
+if rsnpage.text == text:
+    exit("nothing changed")
+
+pywikibot.showDiff(rsnpage.text, text)
+rsnpage.text = text
+summary = cfg["main_page_summary"].format(count)
+print(summary)
+rsnpage.save(summary=summary, minor=False)
+
+for target in archivelist:
+    archivepage = pywikibot.Page(site, cfg["archive_page_name"].format(target[0], target[1]))
+    text = archivepage.text
+    print(archivepage.title())
+    if not archivepage.exists():
+        text = cfg["archive_page_preload"]
+    text += "\n\n" + "\n\n".join(archivelist[target])
+
+    pywikibot.showDiff(archivepage.text, text)
+    archivepage.text = text
+    summary = cfg["archive_page_summary"].format(len(archivelist[target]))
+    print(summary)
+    archivepage.save(summary=summary, minor=False)
---
+++ unblock-zh-log/edit.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 3 Apr 2020 08:08
+# @Author  : Hamish Shing Shing Chau
+# @File    : edit.py
+# @Software: PyCharm
+
+import json
+import os
+import re
+import time
+import requests
+from datetime import datetime
+
+import mwparserfromhell
+os.environ['PYWIKIBOT_DIR'] = os.path.dirname(os.path.realpath(__file__))
+import pywikibot
+# from config import config_page_name  # pylint: disable=E0611,W0614
+
+t1 = datetime.now().microsecond
+t3 = time.mktime(datetime.now().timetuple())
+os.environ['TZ'] = 'UTC'
+print('Starting at: ' + time.asctime(time.localtime(time.time())))
+
+site = pywikibot.Site()
+site.login()
+config_page_name = 'User:Hamish-bot/config1.json'
+config_page = pywikibot.Page(site, config_page_name)
+config_text = config_page.text
+old_config_text = config_page.text
+cfg = json.loads(config_text)
+original_time = cfg['recognized_time']
+print(json.dumps(cfg, indent=4, ensure_ascii=False))
+
+if not cfg["enable"]:
+    exit("disabled\n")
+
+session = requests.Session()
+URL = "https://zh.wikipedia.org/w/api.php"
+REGI_PARAMS = {
+        'action': "query",
+        'format': "json",
+        'list': "logevents",
+        'leprop': "title|timestamp|comment|type",
+        'letype': "newusers",
+        'leuser': "Hamish",
+        'lelimit': "max"
+        }
+IPBE_PARAMS = {
+        'action': "query",
+        'format': "json",
+        'list': "logevents",
+        'leprop': "title|timestamp|comment|type|details",
+        'letype': "rights",
+        'leuser': "Hamish",
+        'lelimit': "max"
+        }
+R = session.post(URL, data=REGI_PARAMS)
+DATA = R.json()
+events = DATA['query']['logevents']
+R = session.post(URL, data=IPBE_PARAMS)
+DATA = R.json()
+events = events + DATA['query']['logevents']
+
+record_page = pywikibot.Page(site, cfg["main_page_name"])
+record_text = record_page.text
+old_record_text = record_page.text
+archivelist = {}
+count = 0
+
+MARK = '<!-- BOT_MARK -->'
+TAG = True
+append_text = ''
+
+for record in events:
+    if record['action'] == 'byemail':
+        timestamp = record['timestamp']
+        if TAG:
+            new_time = timestamp
+            TAG = False
+        print(timestamp, end='\t')
+        action = '註冊'
+        print(action, end='\t')
+        target = record['title'][5:]
+        print(target, end='\t')
+        link_pattern = re.compile(r'unblock-zh/20\d{2}-.+/\d{6}')
+        id_pattern = re.compile(r'\d+')
+        comment = record['comment']
+        prefix = 'https://lists.wikimedia.org/mailman/private/'
+        mid = link_pattern.findall(comment)[0]
+        suffix = '.html'
+        link = prefix + mid + suffix
+        id = id_pattern.findall(link)[1]
+        reason = '[' + link + ' {}]'.format(id)
+        print(id, end='\t')
+        new_log = '|-\n|{}\n|{}\n|{}\n|{}\n'.format(timestamp, action, target, reason)
+        if new_log in old_record_text:
+            continue
+        else:
+            append_text = append_text + new_log
+        print()
+    elif record['type'] == 'rights':
+        if 'unblock-zh' == record['comment'][:10]:
+            timestamp = record['timestamp']
+            if TAG:
+                new_time = timestamp
+                TAG = False
+            print(timestamp, end='\t')
+            action = '授權'
+            print(action, end='\t')
+            target = record['title'][5:]
+            print(target, end='\t')
+            if len(record['comment']) > 10:
+                link_pattern = re.compile(r'unblock-zh/20\d{2}-.+/\d{6}')
+                id_pattern = re.compile(r'\d+')
+                comment = record['comment']
+                prefix = 'https://lists.wikimedia.org/mailman/private/'
+                mid = link_pattern.findall(comment)[0]
+                suffix = '.html'
+                link = prefix + mid + suffix
+                id = id_pattern.findall(link)[1]
+                reason = '[' + link + ' {}]'.format(id)
+                print(id, end='\t')
+                new_log = '|-\n|{}\n|{}\n|{}\n|{}\n'.format(timestamp, action, target, reason)
+                if new_log in old_record_text:
+                    continue
+                else:
+                    append_text = append_text + new_log
+            else:
+                reason = record['comment']
+                print(reason, end='\t')
+                new_log = '|-\n|{}\n|{}\n|{}\n|{}\n'.format(timestamp, action, target, reason)
+                if new_log in old_record_text:
+                    continue
+                else:
+                    append_text = append_text + new_log
+            print()
+
+if new_time == original_time:
+    exit("No change")
+config_text = re.sub(original_time, new_time, config_text)
+append_text = append_text + MARK
+record_text = re.sub(MARK, append_text, record_text)
+config_page.text = config_text
+record_page.text = record_text
+t2 = datetime.now().microsecond
+t4 = time.mktime(datetime.now().timetuple())
+strTime = '(Elasped %dms)' % ((t4 - t3) * 1000 + (t2 - t1) / 1000)
+summary = "Recording new action(s). " + strTime
+print(summary)
+pywikibot.showDiff(old_record_text, record_page.text)
+record_page.save(summary=summary, minor=False)
+pywikibot.showDiff(old_config_text, config_page.text)
+config_page.save(summary=summary, minor=False)
+
