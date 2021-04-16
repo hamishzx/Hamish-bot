ec390b478c6b5cb818bef542d58dcdb29c841f1d
Hamish <hamish.zou@outlook.com>
2021-04-16 19:06:27 +0800

    anm: use function

--- anm-archive/edit.py
+++ anm-archive/edit.py
@@ -11,6 +11,16 @@ os.environ['PYWIKIBOT_DIR'] = os.path.dirname(os.path.realpath(__file__))
 import pywikibot
 from config import config_page_name  # pylint: disable=E0611,W0614
 
+def timeRecord(op, t):
+    user_page = pywikibot.Page(site, "User:Hamish-bot")
+    user_page_text = user_page.text
+    user_page_text = re.sub(r'<!-- T2rs -->(.*)<!-- T2re -->', '<!-- T2rs -->' + t + '<!-- T2re -->', user_page_text, flags=re.M)
+    if op:
+        user_page_text = re.sub(r'<!-- T2os -->(.*)<!-- T2oe -->', '<!-- T2os -->' + t + '<!-- T2oe -->', user_page_text, flags=re.M)
+    pywikibot.showDiff(user_page.text, user_page_text)
+    user_page.text = user_page_text
+    user_page.save(summary = "Updating task report", minor = False)
+
 os.environ['TZ'] = 'UTC'
 print('Starting at: ' + time.asctime(time.localtime(time.time())))
 rec_time = (datetime.datetime.now() + datetime.timedelta(hours = 8)).__format__('%d/%m/%y %H:%M')
@@ -80,6 +90,7 @@ for section in text:
     print()
 
 if count == 0:
+    timeRecord(op, rec_time)
     exit("nothing changed")
 
 pywikibot.showDiff(ewippage.text, mainPageText)
@@ -102,14 +113,4 @@ for target in archivelist:
     summary = cfg["archive_page_summary"].format(len(archivelist[target]))
     print(summary)
     archivepage.save(summary=summary_prefix + summary, minor=False)
-    
-# Updating task table on user page
-
-user_page = pywikibot.Page(site, "User:Hamish-bot")
-user_page_text = user_page.text
-user_page_text = re.sub(r'<!-- T2rs -->(.*)<!-- T2re -->', '<!-- T2rs -->' + rec_time + '<!-- T2re -->', user_page_text, flags=re.M)
-if op:
-    user_page_text = re.sub(r'<!-- T2os -->(.*)<!-- T2oe -->', '<!-- T2os -->' + rec_time + '<!-- T2oe -->', user_page_text, flags=re.M)
-pywikibot.showDiff(user_page.text, user_page_text)
-user_page.text = user_page_text
-user_page.save(summary = "Updating task report", minor = False)
+timeRecord(op, rec_time)
