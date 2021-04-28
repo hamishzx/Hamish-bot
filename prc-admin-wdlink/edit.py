b46da87d05c1724fc426f87fa163a8348284802c
Hamish <hamish.zou@outlook.com>
2021-04-28 22:19:58 +0800

    +city check

--- prc-admin-wdlink/edit.py
+++ prc-admin-wdlink/edit.py
@@ -24,35 +24,75 @@ if (len(province_list) != 34):
     print("[ERR]Isn't it 34?")
     exit
 
-print("Start province check\n")
-province_total = len(province_list)
-province_delta = 0
-for code in province_list:
-    data_page = pywikibot.Page(site, "Template:PRC_admin/data/{0}/00/00/000/000".format(code))
-    text = data_page.text
-    print(code, end='\t')
-    name_pattern = re.compile(r'name=(.*)\|')
-    name = name_pattern.findall(text)[0]
-    width = 20 - len(name.encode("GBK")) + len(name)
-    print(name.ljust(width), end='\t')
-    old_id = wd_id_pattern.findall(text)[0]
-    print(old_id, end='\t')
-    item = pywikibot.ItemPage(repo, old_id)
-    if (item.isRedirectPage()):
-        new_id = item.getRedirectTarget().getID().casefold() # change into lowercase to avoid possible bug
-    else:
-        new_id = old_id
-    print(new_id, end='\t')
-    if (new_id != old_id):
-        province_delta += 1
-        text = re.sub(old_id, new_id, text)
-        pywikibot.showDiff(data_page.text, text)
-        data_page.text = text
-        data_page.save(summary = "Updating wikidata item id from [[:d:{0}|{0}]] to [[:d:{1}|{1}]]".format(old_id, new_id), minor = False)
-    print('\n')
-print('Province check completed.\n{0} total, {1} delta.'.format(province_total, province_delta))
+def province_check(list):
+    print("Start province check\n")
+    province_list = list
+    province_total = len(province_list)
+    province_delta = 0
 
-print("Start city check\n")
+    for province in province_list:
+        out_text = ''
+        data_page = pywikibot.Page(site, "Template:PRC_admin/data/{0}/00/00/000/000".format(province))
+        text = data_page.text
+        out_text += province + '\t'
+        if ("title" in text):
+            name_pattern = re.compile(r'title=(.*)\|')
+        else:
+            name_pattern = re.compile(r'name=(.*)\|')
+        name = name_pattern.findall(text)[0]
+        width = 20 - len(name.encode("GBK")) + len(name)
+        out_text += name.ljust(width) + '\t'
+        old_id = wd_id_pattern.findall(text)[0]
+        out_text += old_id + '\t'
+        province_page = pywikibot.Page(site, name)
+        new_id = pywikibot.ItemPage.fromPage(province_page).getID().casefold()
+        out_text += new_id + '\t'
+        if (new_id != old_id):
+            province_delta += 1
+            text = re.sub(old_id, new_id, text)
+            pywikibot.showDiff(data_page.text, text)
+            data_page.text = text
+            data_page.save(summary = "Updating wikidata item id from [[:d:{0}|{0}]] to [[:d:{1}|{1}]]".format(old_id, new_id), minor = False)
+            out_text += '\n'
+            print(out_text) # print detail only when updated
+    print('Province check completed.\n{0} total, {1} delta.'.format(province_total, province_delta), end='\n')
+
+def city_check(province, list):
+    print("Start city check\n")
+    city_list = list
+    city_total = len(city_list)
+    city_delta = 0
+
+    for city in city_list:
+        out_text = ''
+        data_page = pywikibot.Page(site, "Template:PRC_admin/data/{0}/{1}/00/000/000".format(province, city))
+        text = data_page.text
+        if "fake=" in text:
+            print('[INFO]Skip {0} {1} with fake tag'.format(province, city))
+            continue
+        out_text += province + ' '  + city + '\t'
+        if ("title" in text):
+            name_pattern = re.compile(r'title=(.*)\|')
+        else:
+            name_pattern = re.compile(r'name=(.*)\|')
+        name = name_pattern.findall(text)[0]
+        width = 20 - len(name.encode("GBK")) + len(name)
+        out_text += name.ljust(width) + '\t'
+        old_id = wd_id_pattern.findall(text)[0]
+        out_text += old_id + '\t'
+        city_page = pywikibot.Page(site, name)
+        new_id = pywikibot.ItemPage.fromPage(city_page).getID().casefold()
+        out_text += new_id + '\t'
+        if (new_id != old_id):
+            city_delta += 1
+            text = re.sub(old_id, new_id, text)
+            pywikibot.showDiff(data_page.text, text)
+            data_page.text = text
+            data_page.save(summary = "Updating wikidata item id from [[:d:{0}|{0}]] to [[:d:{1}|{1}]]".format(old_id, new_id), minor = False)
+            out_text += '\n'
+            print(out_text) # print detail only when updated
+    print('City check completed.\n{0} total, {1} delta.'.format(city_total, city_delta), end='\n')
+
 
 end_time = time.time()
 print("Elasped time: " + str(format(end_time - start_time, '.2f') + ' seconds'))
