71f5af342b5cb77892950a03aa1a3de8aafeac91
Hamish <hamish.zou@outlook.com>
2021-04-28 13:20:18 +0800

    +time count, small change

--- prc-admin-wdlink/edit.py
+++ prc-admin-wdlink/edit.py
@@ -6,6 +6,7 @@ import pywikibot
 
 os.environ['TZ'] = 'UTC'
 print('Starting at: ' + time.asctime(time.localtime(time.time())))
+start_time = time.time()
 rec_time = (datetime.datetime.now() + datetime.timedelta(hours = 8)).__format__('%d/%m/%y %H:%M')
 
 site = pywikibot.Site()
@@ -23,8 +24,7 @@ if (len(province_list) != 34):
     print("[ERR]Isn't it 34?")
     exit
 
-# Province Check
-print("Start province check. \n")
+print("Start province check\n")
 province_total = len(province_list)
 province_delta = 0
 for code in province_list:
@@ -51,3 +51,8 @@ for code in province_list:
         data_page.save(summary = "Updating wikidata item id from [[:d:{0}|{0}]] to [[:d:{1}|{1}]]".format(old_id, new_id), minor = False)
     print('\n')
 print('Province check completed.\n{0} total, {1} delta.'.format(province_total, province_delta))
+
+print("Start city check\n")
+
+end_time = time.time()
+print("Elasped time: " + str(format(end_time - start_time, '.2f') + 'seconds'))
