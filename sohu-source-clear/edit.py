68bd689f44df6e343267f3e5deceeaf412ac97da
Hamish Chengcheng Zou <hamish.zou@outlook.com>
2024-04-27 02:13:37 +0800

    sohu: add task
    Signed-off-by: Hamish Chengcheng Zou <hamish.zou@outlook.com>

---
+++ sohu-source-clear/edit.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-       
+
+"""
+# author: Hamish Chengcheng Zou<hamish.zou@dell.com>
+# file  : edit.py.py
+# time  : 27/4/2024 2:01 am
+# desc  : 
+"""
+
+
+import pywikibot
+
+site = pywikibot.Site('zh', 'wikipedia')
+site.login()
+
+todo_list = site.search('insource:"www.sohu.com/a/www.sohu.com/"')
+
+for page in todo_list:
+    text = page.text
+    text = text.replace('www.sohu.com/a/www.sohu.com/', 'www.sohu.com/')
+    page.text = text
+    page.save('去除冗余的sohu链接，[[Special:PermaLink/82374969#清理www.sohu.com/a/www.sohu.com/|BOTREQ]]', minor=True)
+    print(page.title(), 'done')
+    pywikibot.sleep(15)
