cb9f824bccdc6698b8cb95a313fe1c6b95f255e5
CHAU SHING SHING HAMISH <hamishz@toolforge.org>
2024-08-10 09:00:37 +0800

    prc-admin: complete check integrity

---
+++ prc-admin/check.py
@@ -0,0 +1,120 @@
+# ！/usr/bin/env python
+# -*-coding:Utf-8 -*-
+# Time: 7 Aug 2024 12:51
+# Name: check.py
+# Author: CHAU SHING SHING HAMISH
+
+import re
+
+import openpyxl
+import pywikibot
+
+site = pywikibot.Site('zh', 'wikipedia')
+site.login()
+
+
+def show_subdivisions(*args):
+    sheet = args[0]
+    process_admin_name = args[1]
+    subdivisions = []
+
+    if len(args) == 2:
+        # case show city
+        for r in sheet.iter_rows(min_row=2, max_row=sheet.max_row, values_only=True):
+            if r[4:7] == ('00', '000', '000'):
+                subdivisions.append(r)
+        print(process_admin_name + '地级行政区划：')
+        print(subdivisions)
+    elif len(args) == 3:
+        # case show county
+        city_to_show = args[2]
+        for r in sheet.iter_rows(min_row=2, max_row=sheet.max_row, values_only=True):
+            if r[5:7] == ('000', '000') and r[3] == city_to_show:
+                subdivisions.append(r)
+        print(process_admin_name + '县级行政区划：')
+        print(subdivisions)
+    elif len(args) == 4:
+        # case show town
+        city_to_show = args[2]
+        county_to_show = args[3]
+        for r in sheet.iter_rows(min_row=2, max_row=sheet.max_row, values_only=True):
+            if r[6] == '000' and r[3:5] == (city_to_show, county_to_show):
+                subdivisions.append(r)
+        print(process_admin_name + '乡级行政区划：')
+        print(subdivisions)
+    elif len(args) == 5:
+        # case show village
+        city_to_show = args[2]
+        county_to_show = args[3]
+        town_to_show = args[4]
+        for r in sheet.iter_rows(min_row=2, max_row=sheet.max_row, values_only=True):
+            if r[6] != '000' and r[3:6] == (city_to_show, county_to_show, town_to_show):
+                subdivisions.append(r)
+        print(process_admin_name + '村级行政区划：')
+        print(subdivisions)
+    return subdivisions
+
+
+def compare_data(name, data, subs, level):
+    sub_code_sheet = [r[level] for r in subs]
+    data_process = set(sub_code_sheet)
+    subs_process = set(data)
+    shared = data_process & subs_process
+    data_process = [x for x in data_process if x not in shared]
+    subs_process = [x for x in subs_process if x not in shared]
+    if not data_process and not subs_process:
+        print(name + '数据处理结果与页面中一致')
+    else:
+        print(name + '数据处理结果：')
+        if data_process:
+            print(str(data_process) + '是页面中含有但数据中没有的行政区划代码')
+        if subs_process:
+            print(str(subs_process) + '是数据中含有但页面中没有的行政区划代码')
+
+
+def verify_integrity():
+    base_page = pywikibot.Page(site, 'Template:PRC_admin/list/00/00/00/000/000')
+    pattern = re.compile(r'\|(\d{2,3})')
+    provinces = pattern.findall(base_page.text)
+    provinces = ['43']
+    for province in provinces:
+        province_data = openpyxl.load_workbook('Administration_' + province + '.xlsx')
+        province_data_sheet = province_data.active
+        province_list_page = pywikibot.Page(site, 'Template:PRC_admin/list/' + province + '/00/00/000/000')
+        province_data_page = pywikibot.Page(site, 'Template:PRC_admin/data/' + province + '/00/00/000/000')
+        province_name = re.findall(r'name=(.*)\|', province_data_page.text)[0]
+        cities = pattern.findall(province_list_page.text)
+        province_subdivisions = show_subdivisions(province_data_sheet, province_name)
+        compare_data(province_name, cities, province_subdivisions, 3)
+
+        for city in cities:
+            city_list_page = pywikibot.Page(site, 'Template:PRC_admin/list/' + province + '/' + city + '/00/000/000')
+            city_data_page = pywikibot.Page(site, 'Template:PRC_admin/data/' + province + '/' + city + '/00/000/000')
+            city_name = re.findall(r'name=(.*)\|', city_data_page.text)[0]
+            counties = pattern.findall(city_list_page.text)
+            city_subdivisions = show_subdivisions(province_data_sheet, city_name, city)
+            compare_data(city_name, counties, city_subdivisions, 4)
+
+            for county in counties:
+                county_list_page = pywikibot.Page(site,
+                                                  'Template:PRC_admin/list/' + province + '/' + city + '/' + county + '/000/000')
+                county_data_page = pywikibot.Page(site,
+                                                  'Template:PRC_admin/data/' + province + '/' + city + '/' + county + '/000/000')
+                county_name = re.findall(r'name=(.*)\|', county_data_page.text)[0]
+                towns = pattern.findall(county_list_page.text)
+                county_subdivisions = show_subdivisions(province_data_sheet, county_name, city, county)
+                compare_data(county_name, towns, county_subdivisions, 5)
+
+                for town in towns:
+                    town_list_page = pywikibot.Page(site,
+                                                    'Template:PRC_admin/list/' + province + '/' + city + '/' + county + '/' + town + '/000')
+                    town_data_page = pywikibot.Page(site,
+                                                    'Template:PRC_admin/data/' + province + '/' + city + '/' + county + '/' + town + '/000')
+                    town_name = re.findall(r'name=(.*)\|', town_data_page.text)[0]
+                    villages = pattern.findall(town_list_page.text)
+                    town_subdivisions = show_subdivisions(province_data_sheet, town_name, city, county, town)
+                    compare_data(town_name, villages, town_subdivisions, 6)
+
+
+if __name__ == '__main__':
+    verify_integrity()
