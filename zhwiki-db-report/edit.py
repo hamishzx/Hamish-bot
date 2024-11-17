5f98250e7eeedcde1c1298cffde1568579501672
CHAU SHING SHING HAMISH <hamishz@toolforge.org>
2024-11-17 20:22:05 +0800

    zhwiki-db-report: introduce exception

--- zhwiki-db-report/edit.py
+++ zhwiki-db-report/edit.py
@@ -15,7 +15,6 @@ os.environ['TZ'] = 'UTC'
 print('Starting at: ' + time.asctime(time.localtime(time.time())))
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-
 sh_script = os.path.join(dir_path, 'run.sh')
 process = subprocess.Popen(['bash', sh_script, 'report'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
@@ -46,19 +45,24 @@ def time_record(t):
     user_page.text = user_page_text
     user_page.save(summary = "Updating task report", minor = False)
 
-with open(os.path.join(dir_path, '/reports/report1.txt'), 'r') as infile:
+file_path = os.path.join(dir_path, 'reports/report1.txt')
+file_list = ''
+
+with open(file_path, 'r') as infile:
     for line in infile:
         line = line.strip()
         print(line, end='\t')
-        page_text += f'# [[:File:{line}]]'
+        file_list += f'# [[:File:{line}]]'
         if bad_pattern.search(pywikibot.Page(site, 'File:'+line).text):
-            page_text += '（受限制文件）'
+            file_list += '（受限制文件）'
             print('R')
-        page_text += '\n'
+        file_list += '\n'
         print()
 
-
-report_page = pywikibot.Page(site, 'Wikipedia:資料庫報告/檔案描述頁')
-report_page.text = page_text
-report_page.save('[[Wikipedia:机器人/申请/Hamish-bot/8|T8]]：生成本地存在檔案描述的維基共享資源文件列表')
-time_record(datetime.datetime.now() + datetime.timedelta(hours = 8)).__format__('%d/%m/%y %H:%M')
+if not file_list:
+    raise ValueError('No file found')
+else:
+    report_page = pywikibot.Page(site, 'Wikipedia:資料庫報告/檔案描述頁')
+    report_page.text = page_text
+    report_page.save('[[Wikipedia:机器人/申请/Hamish-bot/8|T8]]：生成本地存在檔案描述的維基共享資源文件列表')
+    time_record(str((datetime.datetime.now() + datetime.timedelta(hours=8)).__format__('%d/%m/%y %H:%M')))
